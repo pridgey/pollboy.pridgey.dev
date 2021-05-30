6277e9392aa6d25f6508f15a9d266c4ed4e95511
Pridgey <musicinsilence13@gmail.com>
2021-05-30 01:01:46 -0600

    2.0/ not using this yet

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,11 +1,10 @@
 import { useEffect } from "react";
-import { usePollAPI, useUserID } from "./../../utilities";
+import { usePollAPI } from "./../../utilities";
 import { PollBoyLogo, Button } from "./../../components";
 import { StyledHome } from "./Home.styles";
 
 export const Home = () => {
   const { listPollVotes } = usePollAPI();
-  const userID = useUserID();
 
   useEffect(() => {
     listPollVotes("4421c5fc-9f7f-4309-b26f-6023295ba742").then((result) =>

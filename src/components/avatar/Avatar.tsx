f79cb201c49cd0841ef725bf2e1e8a7475a3333b
pridgey <taylor@pridgey.dev>
2022-12-17 18:09:07 -0700

    solid-pollboy/ add avatar and dropdown

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,16 +1,54 @@
 import type { User } from "@supabase/supabase-js";
 import styles from "./Avatar.module.css";
+import { DropdownOptions } from "../dropdown-options";
+import { createSignal, Show } from "solid-js";
 
 export type AvatarProps = {
   User?: User;
 };
 
 export const Avatar = (props: AvatarProps) => {
+  let avatarRef: HTMLButtonElement;
+
+  const [optionsOpen, setOptionsOpen] = createSignal<boolean>(false);
+
   return (
-    <button type="button" class={styles.avatarbubble}>
-      {props.User?.user_metadata?.username?.split("")?.[0] ||
-        props.User?.email?.split("")?.[0] ||
-        ""}
-    </button>
+    <>
+      <button
+        ref={avatarRef}
+        onClick={() => setOptionsOpen(!optionsOpen())}
+        type="button"
+        class={styles.avatarbubble}
+      >
+        {props.User?.user_metadata?.username?.split("")?.[0] ||
+          props.User?.email?.split("")?.[0] ||
+          ""}
+      </button>
+      <Show when={optionsOpen()}>
+        <DropdownOptions
+          HorizontalAlign="right"
+          VerticalGap={15}
+          PositionRef={avatarRef}
+          OnOutsideClick={() => setOptionsOpen(false)}
+          Options={[
+            {
+              Label: "Hello",
+              OnClick: () => undefined,
+              Icon: "",
+            },
+            {
+              Label: "Logout",
+              OnClick: () => undefined,
+              Icon: "",
+            },
+            {
+              Label: "Delete",
+              OnClick: () => undefined,
+              Icon: "",
+            },
+          ]}
+        />
+      </Show>
+    </>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./avatar";
 export * from "./button";
 export * from "./button-bar";
+export * from "./dropdown-options";
 export * from "./input";
